031019ad9a2f26740a371f6fdd726be1bb57f33d
Dale Ma <dalema22@gmail.com>
2014-10-16 22:44:22 -0600

    db for category

--- db/migrate/20141017044141_create_categories.rb
+++ db/migrate/20141017044141_create_categories.rb
@@ -1,7 +1,9 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-
+    	t.string :name
+    	t.text :description
+    	t.boolean :enabled
       t.timestamps
     end
   end
