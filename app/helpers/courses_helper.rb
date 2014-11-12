746a537de018dff218348796b5c658cfa4f4837e
Dale Ma <dalema22@gmail.com>
2014-11-12 09:43:19 -0700

    add difficulty

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,4 +1,18 @@
 module CoursesHelper
+
+	def course_subtitle(course)
+		# = "<a href=\"#{user_path(c.user.id)}\">#{c.user.name}</a> • #{c.difficulty} • #{c.resources.count} items".html_safe
+		haml_tag :div, class: 'subtitle' do
+			haml_tag :a, href: "#{user_path(course.user.id)}" do
+				haml_concat course.user.name
+			end
+			if course.difficulty.present?
+				haml_concat " • #{course.difficulty}"
+			end
+			haml_concat  " • #{course.resources.count} items"
+		end
+	end
+
 	def self.markdown_to_html(content)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
     markdown.render(content || '')
