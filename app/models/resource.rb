53f3ddde3bddf5417cb382cc4e0e6e5bb31e78d2
Dale Ma <dalema22@gmail.com>
2015-01-04 17:34:56 -0700

    fix resource collection problem

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,6 +2,5 @@ class Resource < ActiveRecord::Base
 	belongs_to :collection
 
   validates :title, length: { minimum: 3, maximum: 255 }, allow_blank: false
-  validates :description, length: { maximum: 140 }
 	validate :url, precense: true
 end
