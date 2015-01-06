8aaba0e53f3a79539f4d05db8b0667533039eca8
Dale Ma <dalema22@gmail.com>
2015-01-05 23:39:17 -0700

    change title cahrs minimum to 2

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,7 +2,7 @@ class Collection < ActiveRecord::Base
   has_many :resources, dependent: :destroy
   belongs_to :user
 
-  validates :name, length: { minimum: 3, maximum: 255 }, allow_blank: false
+  validates :name, length: { minimum: 2, maximum: 255 }, allow_blank: false
   validates :description, length: { maximum: 140 }
   paginates_per 10
 
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,6 @@
 class Resource < ActiveRecord::Base
 	belongs_to :collection
 
-  validates :title, length: { minimum: 3, maximum: 255 }, allow_blank: false
+  validates :title, length: { minimum: 2, maximum: 255 }, allow_blank: false
 	validate :url, precense: true
 end
