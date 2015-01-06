fa135293d61ff2713cb6eda526c4afb1fc2fb199
Dale Ma <dalema22@gmail.com>
2015-01-06 01:07:21 -0700

    dynamic title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def html_title(title="Gain Something")
+    content_for :title, title.to_s
+  end
+
+  def html_description(description)
+    content_for :description, description.to_s
+  end
 end
